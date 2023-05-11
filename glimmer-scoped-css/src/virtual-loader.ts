d8494810ac7e2e58e159fa5a17a37a4993bb0ecc
Buck Doyle <buck.doyle@cardstack.com>
2023-05-11 10:55:42 -0500

    Fix bug with incorrectly-encoded CSS (#12)
    This adds a encodeURIComponent/decodeURIComponent step for the CSS
    that’s embedded in the virtual loader path. This fixes a problem with
    some CSS encodings that resulted in build errors, such as those seen in
    #11. If the base64-encoded CSS contained + this was decoded as a space
    on the other end, resulting in invalid CSS.

--- glimmer-scoped-css/src/virtual-loader.ts
+++ glimmer-scoped-css/src/virtual-loader.ts
@@ -3,33 +3,33 @@ import postcss from 'postcss';
 import scopedStylesPlugin from './postcss-plugin';
 
 export default function virtualLoader(this: LoaderContext<unknown>) {
-  let cssFileAndSelectorString = this.loaders[this.loaderIndex]?.options;
+  let encodedCssAndSelectorString = this.loaders[this.loaderIndex]?.options;
 
-  if (typeof cssFileAndSelectorString !== 'string') {
+  if (typeof encodedCssAndSelectorString !== 'string') {
     throw new Error(
-      `glimmer-scoped-css/src/virtual-loader received unexpected request: ${cssFileAndSelectorString}`
+      `glimmer-scoped-css/src/virtual-loader received unexpected request: ${encodedCssAndSelectorString}`
     );
   }
 
-  let cssFileAndSelector = new URLSearchParams(cssFileAndSelectorString);
+  let encodedCssAndSelector = new URLSearchParams(encodedCssAndSelectorString);
 
-  let cssFile = cssFileAndSelector.get('file');
+  let encodedCss = encodedCssAndSelector.get('file');
 
-  if (!cssFile) {
+  if (!encodedCss) {
     throw new Error(
-      `glimmer-scoped-css/src/virtual-loader missing file parameter: ${cssFileAndSelectorString}`
+      `glimmer-scoped-css/src/virtual-loader missing file parameter: ${encodedCssAndSelectorString}`
     );
   }
 
-  let cssSelector = cssFileAndSelector.get('selector');
+  let cssSelector = encodedCssAndSelector.get('selector');
 
   if (!cssSelector) {
     throw new Error(
-      `glimmer-scoped-css/src/virtual-loader missing selector parameter: ${cssFileAndSelectorString}`
+      `glimmer-scoped-css/src/virtual-loader missing selector parameter: ${encodedCssAndSelectorString}`
     );
   }
 
-  let cssSource = atob(cssFile);
+  let cssSource = atob(decodeURIComponent(encodedCss));
   let result = postcss([scopedStylesPlugin(cssSelector)]).process(cssSource);
 
   return result.css;
