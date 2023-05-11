d4383cc38fd1de5f6d8f079c3e61786dcf6211aa
Buck Doyle <buck.doyle@cardstack.com>
2023-05-11 12:14:46 -0400

    Remove code for unsupported features

--- glimmer-scoped-css/src/postcss-plugin.ts
+++ glimmer-scoped-css/src/postcss-plugin.ts
@@ -112,34 +112,19 @@ function rewriteSelector(
   id: string,
   selector: selectorParser.Selector,
   selectorRoot: selectorParser.Root,
-  slotted = false
 ) {
   let node: selectorParser.Node | null = null
   let shouldInject = true
   // find the last child node to insert attribute selector
   selector.each(n => {
-    // DEPRECATED ">>>" and "/deep/" combinator
-    if (
-      n.type === 'combinator' &&
-      (n.value === '>>>' || n.value === '/deep/')
-    ) {
-      n.value = ' '
-      n.spaces.before = n.spaces.after = ''
-      warn(
-        `the >>> and /deep/ combinators have been deprecated. ` +
-          `Use :deep() instead.`
-      )
-      return false
-    }
-
     if (n.type === 'pseudo') {
       const { value } = n
-      // deep: inject [id] attribute at the node before the ::v-deep
+      // deep: inject [id] attribute at the node before the ::deep
       // combinator.
-      if (value === ':deep' || value === '::v-deep') {
+      if (value === ':deep') {
         if (n.nodes.length) {
-          // .foo ::v-deep(.bar) -> .foo[xxxxxxx] .bar
-          // replace the current node with ::v-deep's inner selector
+          // .foo :deep(.bar) -> .foo[xxxxxxx] .bar
+          // replace the current node with :deep's inner selector
           let last: selectorParser.Selector['nodes'][0] = n
           n.nodes[0]!.each(ss => {
             selector.insertAfter(last, ss)
@@ -156,43 +141,13 @@ function rewriteSelector(
             )
           }
           selector.removeChild(n)
-        } else {
-          // DEPRECATED usage
-          // .foo ::v-deep .bar -> .foo[xxxxxxx] .bar
-          warn(
-            `::v-deep usage as a combinator has ` +
-              `been deprecated. Use :deep(<inner-selector>) instead.`
-          )
-          const prev = selector.at(selector.index(n) - 1)
-          if (prev && isSpaceCombinator(prev)) {
-            selector.removeChild(prev)
-          }
-          selector.removeChild(n)
         }
         return false
       }
 
-      // slot: use selector inside `::v-slotted` and inject [id + '-s']
-      // instead.
-      // ::v-slotted(.foo) -> .foo[xxxxxxx-s]
-      if (value === ':slotted' || value === '::v-slotted') {
-        rewriteSelector(id, n.nodes[0]!, selectorRoot, true /* slotted */)
-        let last: selectorParser.Selector['nodes'][0] = n
-        n.nodes[0]!.each(ss => {
-          selector.insertAfter(last, ss)
-          last = ss
-        })
-        // selector.insertAfter(n, n.nodes[0])
-        selector.removeChild(n)
-        // since slotted attribute already scopes the selector there's no
-        // need for the non-slot attribute.
-        shouldInject = false
-        return false
-      }
-
       // global: replace with inner selector and do not inject [id].
-      // ::v-global(.foo) -> .foo
-      if (value === ':global' || value === '::v-global') {
+      // ::global(.foo) -> .foo
+      if (value === ':global') {
         selectorRoot.insertAfter(selector, n.nodes[0]!)
         selectorRoot.removeChild(selector)
         return false
@@ -214,14 +169,13 @@ function rewriteSelector(
   }
 
   if (shouldInject) {
-    const idToAdd = slotted ? id + '-s' : id
     selector.insertAfter(
       // If node is null it means we need to inject [id] at the start
       // insertAfter can handle `null` here
       node as any,
       selectorParser.attribute({
-        attribute: idToAdd,
-        value: idToAdd,
+        attribute: id,
+        value: id,
         raws: {},
         quoteMark: `"`
       })
